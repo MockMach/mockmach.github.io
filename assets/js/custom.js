6dfc54160bb26bb3d530780223b87959348fccdd
Derek Nutile <dereknutile@gmail.com>
2014-09-27 10:06:32 -0700

    Fixed jekyll build bug.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,51 @@
 
 $(document).ready(function(){
-  // cool code goes here!
+  $(window).scroll(function() {
+    
+    $('#phone').each(function(){
+      var imagePos = $(this).offset().top;
+      
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+          $(this).addClass("slideUp");
+        }
+    });
+
+    $('#pin').each(function(){
+      var imagePos = $(this).offset().top;
+      
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+          $(this).addClass("hatch");
+        }
+    });
+
+    $('#pop-up-message').each(function(){
+      var imagePos = $(this).offset().top;
+      
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+          $(this).addClass("slideExpandUp");
+        }
+    });   
+
+    $('#example-4').each(function(){
+      var imagePos = $(this).offset().top;
+      
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+          $('.device-arrow').addClass("stretchRight");
+        }
+    });   
+
+    $('#example-5').each(function(){
+      var imagePos = $(this).offset().top;
+      
+      var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow+400) {
+          $('.graph-bar').addClass("pullUp");
+        }
+    });
+
+  });
 });
